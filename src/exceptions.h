1c2b29b6c92a0b12ae8866aca7c22dbfe59cf2d6
Justin Talbot <justintalbot@gmail.com>
2011-04-27 15:56:10 -0700

    Add missing exceptions header

---
+++ src/exceptions.h
@@ -0,0 +1,33 @@
+
+#ifndef RIPOSTE_EXCEPTIONS_H
+#define RIPOSTE_EXCEPTIONS_H
+
+class RiposteException {
+public:
+	RiposteException() {}
+	virtual std::string what() const = 0;
+};
+
+// An R-level error
+class RiposteError : public RiposteException {
+	std::string message;
+public:
+	RiposteError(std::string m) : RiposteException(), message(m) {}
+	std::string what() const { return message; }
+};
+
+class CompileError : public RiposteException {
+	std::string message;
+public:
+	CompileError(std::string m) : RiposteException(), message(m) {}
+	std::string what() const { return message; }
+};
+
+class RuntimeError : public RiposteException {
+	std::string message;
+public:
+	RuntimeError(std::string m) : RiposteException(), message(m) {}
+	std::string what() const { return message; }
+};
+
+#endif
