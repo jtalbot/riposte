816d475871157f87cc59625698bd337b85ba9da0
Justin Talbot <justintalbot@gmail.com>
2011-09-07 16:41:08 -0700

    Revised B-S

--- tests/black_scholes.R
+++ tests/black_scholes.R
@@ -7,12 +7,13 @@ T <- rep(2, 1,N_OPTIONS)
 r <- rep(.02,1,N_OPTIONS)
 v <- rep(5,1,N_OPTIONS)
 invSqrt2Pi <- 0.39894228040
+log10 <- log(10)
 
 CND <- function(X) {
     L <- abs(X)
     k <- 1.0 / (1.0 + 0.2316419 * L)
     w <- (((((1.330274429*k) - 1.821255978)*k + 1.781477937)*k - 0.356563782)*k + 0.31938153)*k
-    w <- w * invSqrt2Pi * exp(-L * L * .5)
+    w <- w * invSqrt2Pi * exp(L * L * -.5)
     #Xgt0 <- X > 0
     #w[Xgt0] <- (1-w)[Xgt0]
     w
@@ -23,7 +24,7 @@ blackScholes <- function() {
 	acc <- 0
 	for(i in 1:N_BLACK_SCHOLES_ROUNDS) {
 		delta <- v * sqrt(T)
-		d1 <- (log10(S/X) + (r + v * v * .5) * T) / delta
+		d1 <- (log(S/X)/log10 + (r + v * v * .5) * T) / delta
 		d2 <- d1 - delta
 		acc <- acc + sum(S * CND(d1) - X * exp(-r * T) * CND(d2))
 	}
