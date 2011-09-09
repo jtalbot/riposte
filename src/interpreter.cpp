231007799d143d43033f4baf0ee0525888362c6d
Zach DeVito <zdevito@gmail.com>
2011-09-09 15:49:20 -0700

    Recorder will start when given a vector and scalar op.

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -466,12 +466,12 @@ bool isRecordable(Value const& a) {
 		&& a.length % TRACE_VECTOR_WIDTH == 0;
 }
 bool isRecordable(Value const& a, Value const& b) {
-	return (a.isDouble() || a.isInteger())
-		&& (b.isDouble() || b.isInteger())
-		&& a.length > TRACE_VECTOR_WIDTH
-		&& a.length % TRACE_VECTOR_WIDTH == 0
-		&& a.length == b.length;
-
+	bool valid_types =   (a.isDouble() || a.isInteger())
+				      && (b.isDouble() || b.isInteger());
+	size_t length = std::max(a.length,b.length);
+	bool compatible_lengths = a.length == 1 || b.length == 1 || a.length == b.length;
+	bool should_record_length = length > TRACE_VECTOR_WIDTH && length % TRACE_VECTOR_WIDTH == 0;
+	return valid_types && compatible_lengths && should_record_length;
 }
 
 
