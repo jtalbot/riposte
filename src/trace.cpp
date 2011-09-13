e26aa52389cc6395d8539462865006af3e880d07
Zach DeVito <zdevito@gmail.com>
2011-09-12 17:19:14 -0700

    Added a pathway to arbb from trace recorder
    This is disabled by default but can be enabled to compare
    the performance of the trace interpreter against arbb's JIT.

--- src/trace.cpp
+++ src/trace.cpp
@@ -11,19 +11,33 @@ void Trace::reset() {
 	max_live_register = NULL;
 }
 
+std::string Trace::toString(State & state) {
+	std::ostringstream out;
+	out << "recorded: \n";
+	for(size_t j = 0; j < n_nodes; j++) {
+		IRNode & node = nodes[j];
+		out << "n" << j << ": " << node.toString() << "\n";
+	}
+	out << "outputs: \n";
+	for(size_t i = 0; i < n_outputs; i++) {
 
-static void print_regs(int foo) {
-	for(int i = 0; i < 32; i++)
-		if( foo & (1 << i))
-			printf("1");
-		else
-			printf("0");
-	printf("\n");
+		Output & o = outputs[i];
+		switch(o.location.type) {
+		case Trace::Location::REG:
+			out << "r" << o.location.id; break;
+		case Trace::Location::SLOT:
+			out << "s" << o.location.id; break;
+		case Trace::Location::VAR:
+			out << "v" << o.location.id; break;
+		}
+		out << "(" << o.location.environment << ") = n" << o.ref << "\n";
+	}
+	return out.str();
 }
 
 #define REG(state, i) (*(state.base+i))
 
-static const Value & get_output_value(State & state, const Trace::Location & l) {
+static const Value & get_location_value(State & state, const Trace::Location & l) {
 	switch(l.type) {
 	case Trace::Location::REG:
 		return l.base[l.id];
@@ -34,7 +48,7 @@ static const Value & get_output_value(State & state, const Trace::Location & l) 
 		return l.environment->hget(Symbol(l.id));
 	}
 }
-static void set_output_value(State & state, const Trace::Location & l, const Value & v) {
+static void set_location_value(State & state, const Trace::Location & l, const Value & v) {
 	switch(l.type) {
 	case Trace::Location::REG:
 		l.base[l.id] = v;
@@ -57,6 +71,45 @@ static bool is_location_dead(Trace & trace, const Trace::Location & l) {
 	);
 }
 
+//attempts to find a future at location l, returns true if the location is live and contains a future
+static bool get_location_value_if_live(State & state, Trace & trace, const Trace::Location & l, Value & v) {
+	if(is_location_dead(trace,l))
+		return false;
+	v = get_location_value(state,l);
+	return v.isFuture();
+}
+
+
+//bit-string based allocator for registers
+typedef uint32_t Allocator;
+static inline void Allocator_init(Allocator * a) {
+	*a = ~0;
+}
+
+static inline void Allocator_print(Allocator * a) {
+	int foo = *a;
+	for(int i = 0; i < 32; i++)
+		if( foo & (1 << i))
+			printf("-");
+		else
+			printf("a");
+	printf("\n");
+}
+
+
+static inline int Allocator_allocate(Allocator * a) {
+	int reg = ffs(*a) - 1;
+	*a &= ~(1 << reg);
+	return reg;
+}
+
+static inline void Allocator_free(Allocator * a, int reg) {
+	*a |= (1 << reg);
+}
+
+
+//#define USE_ARBB_TRACE_EXECUTE
+#ifndef USE_ARBB_TRACE_EXECUTE
 void Trace::execute(State & state) {
 
 	//check list of recorded output locations for live futures
@@ -65,13 +118,12 @@ void Trace::execute(State & state) {
 	for(size_t i = 0; i < n_outputs; ) {
 		Output & o = outputs[i];
 
-		const Value & loc = get_output_value(state,o.location);
-		if(is_location_dead(*this,o.location) ||
-		   loc.header != Type::Future ) {
+		Value loc;
+		if(!get_location_value_if_live(state,*this,o.location,loc)) {
 			o = outputs[--n_outputs];
 		} else {
 			IRef ref = loc.future.ref;
-			o.ref = ref; //only for prettying printing...
+			o.ref = ref; //only for pretty printing...
 			Type::Enum typ = loc.future.typ;
 			nodes[ref].r_external = true;
 			if(nodes[ref].r.p == NULL) //if this is the first VM value that refers to this output, allocate space for it in the VM
@@ -79,7 +131,7 @@ void Trace::execute(State & state) {
 			Value v;
 			Value::Init(v,typ,length);
 			v.p = nodes[ref].r.p;
-			set_output_value(state,o.location,v);
+			set_location_value(state,o.location,v);
 			i++;
 		}
 	}
@@ -91,29 +143,22 @@ void Trace::execute(State & state) {
 	//when we encounter the first use we allocate a register
 	//when we encounter the def, we free that register
 
-	uint32_t free_regs = ~0;
+	Allocator free_reg;
+	Allocator_init(&free_reg);
 	for(size_t i = n_nodes; i > 0; i--) {
 		IRNode & n = nodes[i - 1];
 		if(!n.r_external) { //outputs do not get assigned registers, we use the memory previous allocated for them to hold intermediates
 			//handle the def of node n by freeing allocated register
 			int reg = (n.r.p - registers[0]) / TRACE_VECTOR_WIDTH;
-			//printf("freeing register %d\n",reg);
-			//printf("n%d: reg%d\n",i,reg);
-			free_regs |= (1 << reg);
-			//print_regs(free_regs);
+			Allocator_free(&free_reg,reg);
 		}
 		if( n.usesRegA() ){ //a is a register, handle the use of a
 			IRNode & def = nodes[n.a.i];
 			if(def.r_external) { //since 'a' refers to an output, the interpreter will need to advance the memory reference on each iteration
-				                //we set a's type to I_VECTOR so it knows to do this
+				                //we set a_external so the interpreter knows to advance the pointer
 				n.a_external = true;
 			} else if(def.r.p == NULL) { //no register has be assigned to the def. This is the first encountered use so we allocate a register for it
-				//allocate a register
-				//TODO: we need to make sure we can't run out of registers
-				int reg = ffs(free_regs) - 1;
-				//printf("n%d = register %d\n",n.a.i,reg);
-				free_regs &= ~(1 << reg);
-				//print_regs(free_regs);
+				int reg = Allocator_allocate(&free_reg);
 				def.r.p = registers[reg];
 			}
 			//replace the reference to the node with the pointer to where the values will be stored, this is either a register, part of an input array, or part of an output array
@@ -125,12 +170,7 @@ void Trace::execute(State & state) {
 			if(def.r_external) {
 				n.b_external = true;
 			} else if (def.r.p == NULL) {
-				//allocate a register
-				//TODO: we need to make sure we can't run out of register
-				int reg = ffs(free_regs) - 1;
-				//printf("n%d = register %d\n",n.b.i,reg);
-				free_regs &= ~(1 << reg);
-				//print_regs(free_regs);
+				int reg = Allocator_allocate(&free_reg);
 				def.r.p = registers[reg];
 			}
 			n.b.p = def.r.p;
@@ -184,27 +224,186 @@ void Trace::execute(State & state) {
 		}
 	}
 }
+#else
 
-std::string Trace::toString(State & state) {
-	std::ostringstream out;
-	out << "recorded: \n";
-	for(size_t j = 0; j < n_nodes; j++) {
-		IRNode & node = nodes[j];
-		out << "n" << j << ": " << node.toString() << "\n";
+
+
+#include <arbb_vmapi.h>
+
+
+
+#define ARBB_RUN(fn) \
+	do { \
+			/*printf("%s:%d: %s\n",__FILE__,__LINE__,#fn);*/ \
+			arbb_error_t e = (fn); \
+			if(arbb_error_none != e) \
+					panic(__FILE__,__LINE__,#fn); \
+	} while(0)
+
+
+//void arbb_set_error_details_null(arbb_error_details_t * d)  { *d = NULL; }
+struct ArbbState {
+
+	arbb_context_t ctx;
+	arbb_error_details_t details;
+	State & state;
+	Trace & trace;
+	arbb_function_t function;
+	std::map<void *,arbb_global_variable_t> output_variables;
+	std::map<void *,arbb_global_variable_t> input_variables;
+	std::vector<arbb_binding_t> bindings;
+	arbb_variable_t result_vars[TRACE_MAX_NODES];
+
+	ArbbState(State & s, Trace & t)
+	: state(s), trace(t) {
+		arbb_set_error_details_null(&details);
+		arbb_get_default_context(&ctx,&details);
 	}
-	out << "outputs: \n";
-	for(size_t i = 0; i < n_outputs; i++) {
 
-		Output & o = outputs[i];
-		switch(o.location.type) {
-		case Trace::Location::REG:
-			out << "r" << o.location.id; break;
-		case Trace::Location::SLOT:
-			out << "s" << o.location.id; break;
-		case Trace::Location::VAR:
-			out << "v" << o.location.id; break;
+	arbb_global_variable_t addGlobal(IROp::Type typ, void * v) {
+		arbb_type_t arbb_typ = getType(typ,false);
+		uint64_t length = trace.length;
+		uint64_t width = sizeof(double);
+		arbb_binding_t binding;
+		ARBB_RUN(arbb_create_dense_binding(ctx,&binding,v,1,&length,&width,&details));
+		arbb_global_variable_t ov;
+		ARBB_RUN(arbb_create_global(ctx,&ov,arbb_typ,NULL,binding,NULL,&details));
+		bindings.push_back(binding);
+		return ov;
+	}
+
+	arbb_opcode_t getOp(IROpCode::Enum opcode) {
+#define GET_ARBB(op,str,nm) case IROpCode::op : return arbb_op_##op;
+		switch(opcode) {
+			IR_BINARY(GET_ARBB)
+#define arbb_op_ceiling arbb_op_ceil
+#define arbb_op_log arbb_op_ln
+			IR_UNARY(GET_ARBB)
+#undef arbb_op_ceiling
+#undef arbb_op_log
+			case IROpCode::coerce: return arbb_op_cast;
+			default: _error("unknown op");
 		}
-		out << "(" << o.location.environment << ") = n" << o.ref << "\n";
 	}
-	return out.str();
+
+	arbb_variable_t getVar(IROp::Type typ, IROp::Encoding enc, bool isExternal, IRNode::InputReg & reg) {
+		arbb_global_variable_t gv;
+		if(isExternal)
+			gv = input_variables[reg.p];
+		else if(IROp::E_SCALAR == enc) {
+			arbb_type_t atyp = getType(typ,true);
+			ARBB_RUN(arbb_create_constant(ctx,&gv,atyp,&reg.d,NULL,&details));
+		} else {
+			return result_vars[reg.i];
+		}
+		arbb_variable_t v;
+		ARBB_RUN(arbb_get_variable_from_global(ctx,&v,gv,&details));
+		return v;
+	}
+
+	void run() {
+		//find live outputs and create arbb global variables to hold them
+		for(size_t i = 0; i < trace.n_outputs; ) {
+			Trace::Output & o = trace.outputs[i];
+			Value loc;
+			if(!get_location_value_if_live(state,trace,o.location,loc)) {
+				o = trace.outputs[--trace.n_outputs];
+			} else {
+				IRef ref = loc.future.ref;
+				IRNode & def = trace.nodes[ref];
+				o.ref = ref; //only for pretty printing...
+				Type::Enum typ = loc.future.typ;
+				def.r_external = true;
+				if(def.r.p == NULL) {
+					//if this is the first VM value that refers to this output, allocate space for it in the interpreter, and create a variable bound to it in the arbb vm
+					def.r.p = new (PointerFreeGC) double[trace.length];
+					output_variables[def.r.p] = addGlobal(typ == Type::Integer ? IROp::T_INT : IROp::T_DOUBLE, def.r.p);
+				}
+				Value v;
+				Value::Init(v,typ,trace.length);
+				v.p = def.r.p;
+				set_location_value(state,o.location,v);
+				i++;
+			}
+		}
+
+		printf("executing trace:\n%s\n",trace.toString(state).c_str());
+
+		//find inputs and create arbb variables bound to them
+		for(size_t i = 0; i < trace.n_nodes; i++) {
+			IRNode & node = trace.nodes[i];
+			if(node.a_external && input_variables.count(node.a.p) == 0) {
+				input_variables[node.a.p] = addGlobal(node.op.a_typ,node.a.p);
+			}
+			if(node.b_external && input_variables.count(node.b.p) == 0) {
+				input_variables[node.b.p] = addGlobal(node.op.b_typ,node.b.p);
+			}
+		}
+		//emit arbb function
+		arbb_type_t fn_typ;
+		ARBB_RUN(arbb_get_function_type(ctx,&fn_typ,0,NULL,0,NULL,&details));
+		ARBB_RUN(arbb_begin_function(ctx,&function,fn_typ,NULL,true,&details));
+		for(size_t i = 0; i < trace.n_nodes; i++) {
+			IRNode & node = trace.nodes[i];
+			arbb_type_t rtyp = getType(std::max(node.op.a_typ,node.op.b_typ),false);
+			ARBB_RUN(arbb_create_local(function,&result_vars[i],rtyp,NULL,&details));
+			arbb_variable_t inputs[2];
+			inputs[0] = getVar(node.op.a_typ,node.op.a_enc,node.a_external,node.a);
+			if(IROpCode_is_binary(node.op.code))
+				inputs[1] = getVar(node.op.b_typ,node.op.b_enc,node.b_external,node.b);
+			ARBB_RUN(arbb_op(function,getOp(node.op.code),&result_vars[i],inputs,NULL,NULL,&details));
+			if(node.r_external) {
+				arbb_variable_t o;
+				ARBB_RUN(arbb_get_variable_from_global(ctx,&o,output_variables[node.r.p],&details));
+				ARBB_RUN(arbb_op(function,arbb_op_copy,&o,&result_vars[i],NULL,NULL,&details));
+			}
+		}
+
+
+		ARBB_RUN(arbb_end_function(function,&details));
+		{
+			arbb_string_t fn_str;
+			ARBB_RUN(arbb_serialize_function(function,&fn_str,&details));
+			printf("function is\n%s\n",arbb_get_c_string(fn_str));
+			arbb_free_string(fn_str);
+		}
+
+		//compile and warm up the vm
+		ARBB_RUN(arbb_execute(function,NULL,NULL,&details));
+
+		//now some timing
+		timespec begin = get_time();
+		ARBB_RUN(arbb_execute(function,NULL,NULL,&details));
+		print_time_elapsed("arbb exec",begin);
+
+	}
+	arbb_type_t getType(IROp::Type typ, bool isScalar) {
+		arbb_scalar_type_t st;
+		switch(typ) {
+		case IROp::T_INT: st = arbb_i64; break;
+		case IROp::T_DOUBLE: st = arbb_f64; break;
+		default: _error("unsupported type");
+		}
+		arbb_type_t arbb_typ;
+		ARBB_RUN(arbb_get_scalar_type(ctx,&arbb_typ,st,&details));
+		if(isScalar) {
+			return arbb_typ;
+		} else {
+			arbb_type_t vec_typ;
+			ARBB_RUN(arbb_get_dense_type(ctx,&vec_typ,arbb_typ,1,&details));
+			return vec_typ;
+		}
+	}
+	void panic(const char * fname, int lineno, const char * line) {
+		char err[1024];
+		snprintf(err,1024,"%s:%d:%s: %s\n",fname,lineno,line,arbb_get_error_message(details));
+		_error(err);
+	}
+};
+
+
+void Trace::execute(State & state) {
+	ArbbState arbb(state,*this);
+	arbb.run();
 }
+#endif
