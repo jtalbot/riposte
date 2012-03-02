36b80be33dd5831466ae4a44b5d6b0f83b3b0b38
Zach DeVito <zdevito@stanford.edu>
2012-03-01 21:23:47 -0800

    reference kmeans implementation.

--- benchmarks/kmeans.R
+++ benchmarks/kmeans.R
@@ -1,14 +1,16 @@
 
 library(MASS)
 
-a <- rbind(
-	mvrnorm(100,c(0,0), matrix(c(1,0,0,1), 2,2)),
-	mvrnorm(100,c(4,0), matrix(c(0.5,0,0,0.5), 2,2)),
-	mvrnorm(100,c(0,4), matrix(c(1,0,0,1), 2,2)),
-	mvrnorm(100,c(2,2), matrix(c(0.25,0.2,0.2,0.25), 2,2)),
-	mvrnorm(100,c(1,0), matrix(c(0.25,0.22,0.22,0.25), 2,2))
-	)
-
+#a <- rbind(
+#	mvrnorm(100,c(0,0), matrix(c(1,0,0,1), 2,2)),
+#	mvrnorm(100,c(4,0), matrix(c(0.5,0,0,0.5), 2,2)),
+#	mvrnorm(100,c(0,4), matrix(c(1,0,0,1), 2,2)),
+#	mvrnorm(100,c(2,2), matrix(c(0.25,0.2,0.2,0.25), 2,2)),
+#	mvrnorm(100,c(1,0), matrix(c(0.25,0.22,0.22,0.25), 2,2))
+#	)
+a <- read.table("/Users/zdevito/riposte/benchmarks/kmeans.txt")[[1]]
+dim(a) <- c(500,2)
+print(a)
 means <- list(
 	a[1,],
 	a[2,],
