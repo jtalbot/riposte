83383b970203542ed2c2db613daaf911b345b104
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-09-27 01:59:41 -0700

    outputfixing

--- src/trace_llvm_compiler.cpp
+++ src/trace_llvm_compiler.cpp
@@ -2154,7 +2154,7 @@ struct TraceLLVMCompiler {
         llvm::BitstreamWriter Stream(BitCodeBuf);
         
         BitCodeBuf.reserve(256*1024);
-        //llvm::WriteBitcodeToStream(mainModule, Stream);
+        llvm::WriteBitcodeToStream(mainModule, Stream);
 #endif /* USE_TEXT_NVVM_INTERFACE */
         
         // generate PTX
