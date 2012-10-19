b0c4b99cba5fdb65d05adb8945111f0d0e97a1a7
Justin Talbot <justintalbot@gmail.com>
2012-10-19 12:25:13 -0700

    Fix binary search

--- bbenchmarks/binarysearch.R
+++ bbenchmarks/binarysearch.R
@@ -13,12 +13,12 @@ K <- 10000000L
 a <- 1:K
 
 binary.search <- function(v, key) {
-    a <- rep(1L, length(key))
-    b <- rep(length(v), length(key))
+    a <- 1L
+    b <- K 
 
     while(any(a < b)) {
         i <- (a+b) %/% 2L
-        a <- ifelse(v[i] < key, i+1L, i)
+        a <- ifelse(v[i] < key, i+1L, a)
         b <- ifelse(v[i] < key, b, i)
     }
     return(a)
@@ -27,7 +27,7 @@ binary.search <- function(v, key) {
 run <- function() {
     for(i in 1L:N) {
         s <- i*(K/N)
-        j <- binary.search(a, s:(s+(M-1)))
+        j <- binary.search(a, s:(s+M-1))
     }
 }
 
