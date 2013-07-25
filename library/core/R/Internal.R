ee3d495af72671c25136dd25aaa555a2efa80ebe
Justin Talbot <justintalbot@gmail.com>
2013-07-25 01:03:19 -0700

    Added file that somehow disappeared from git's file tracking

---
+++ library/core/R/Internal.R
@@ -0,0 +1,13 @@
+
+.Internal <- function(call) {
+    call <- .pr_expr(.getenv(NULL), 'call')
+    internal <- getRegisteredNamespace('internal') 
+    if(.env_exists(internal, strip(call[[1]]))) {
+        call[[1]] <- internal[[strip(call[[1]])]]
+        promise('call', call, .frame(1L)[[1L]], .getenv(NULL))
+        call
+    }
+    else {
+        stop(.concat(list("there is no .Internal function '", call[[1]], "'")))
+    }
+}
